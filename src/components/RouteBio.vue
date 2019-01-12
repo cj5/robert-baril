10379ca1c3ba9a51f880deb7d12c5e284b78441d
Chris <stac1221@gmail.com>
2019-01-12 00:30:57 -0600

    Marking up biography route

--- src/components/RouteBio.vue
+++ src/components/RouteBio.vue
@@ -1,8 +1,36 @@
 <template>
-  <div class="header-spacing">
-    <div class="py-5">
+  <div>
+    <div class="ribbon bg-black pt-100 d-flex justify-content-center">
+      <img src="../assets/images/baril-ribbon.jpg" alt="">
+    </div>
+    <div class="py-8">
       <div class="container">
-        <p>RouteBio</p>
+        <h2 class="text-center mb-8">Robert Baril. Comic. Lover. Friend.</h2>
+        <div class="row content mb-8">
+          <div class="col-md-6 pl-md-0">
+            <img src="../assets/images/baril-stand-up.jpg" alt="Robert Baril stand up" class="w-100">
+          </div>
+          <div class="col-md-6 pr-md-0 d-flex align-items-center">
+            <p class="">A philosophy and political science major in college, Robert uses his sharp wit to cut through the most dense and divisive issues of the day.  His trademark brand of smart, lacerating humor has made him one of the fastest rising comedians in the Twin Cities, and garnered him his own radio show on AM 950, Laughing Matters with Robert Baril,  Additionally, Robert also draws on his experiences as a substitute teacher for his scathing material on the educational system, children, and society at large.</p>
+          </div>
+        </div>
+        <div class="content tweets">
+          <h3 class="text-center mb-5">Latest Tweets (from Twitter API)</h3>
+          <div class="d-flex justify-content-center">
+            <ul>
+              <li>Latest tweet 1</li>
+              <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, nemo!</li>
+              <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis nostrum eum possimus aut.</li>
+              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus in doloremque ab!</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim!</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, debitis.</li>
+              <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, a itaque.</li>
+              <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore.</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>

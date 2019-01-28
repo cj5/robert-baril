1dfd08dc7638a22b847d686b73aae99f79b12422
Chris <stac1221@gmail.com>
2019-01-27 23:25:20 -0600

    added _mq.scss for media queries. started responsive layout of home page upcoming shows component

--- src/components/RouteHome.vue
+++ src/components/RouteHome.vue
@@ -11,7 +11,7 @@
           <iframe width="100%" height="100%" src="https://www.youtube.com/embed/4A-ECbAqfa4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
         <div class="text-center mt-5">
-          <router-link to="/media">More from Robert ></router-link>
+          <router-link to="/media" class="fancy-underline">More from Robert ></router-link>
         </div>
       </div>
     </section>
